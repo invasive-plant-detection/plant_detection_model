f2476e1d142a8c40710636f213408c02cfd7fc5f
Simon Egli <eglisi1@students.zhaw.ch>
2024-01-15 09:59:50 +0100

    reformat mai.py

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 
 # pylint: disable=E0401
 from app.schemas.schemas import PredictionRequestModel, PredictionResponseModel
+
 # pylint: disable=E0401
 from app.services.prediction_service import PredictionService
 
@@ -18,6 +19,7 @@ app = FastAPI()
 def read_root(request: PredictionRequestModel) -> PredictionResponseModel:
     "Predicts the class of the image sent in the request"
     return prediction_service.predict(request)
+
 
 @app.get("/health")
 def health_check() -> Dict:
